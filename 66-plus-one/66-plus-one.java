22d3f4f5ada38c0745f3f6017d4e4f0abdc91e7f
이재석 <34270397+seok-2-o@users.noreply.github.com>
2022-08-07 21:04:49 +0900

    Time: 0 ms (100.00%), Space: 41.7 MB (84.37%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -1,36 +1,23 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int length = digits.length;
-        int [] result = new int [length];
-        
-        int carry = 0;
-        int add = 1;
 
+        int length = digits.length;
+        int carry = 1;
 
         for(int idx = length - 1; idx >= 0; idx--) {
-            int current = digits[idx] + carry + add;
-            if(current > 9) {
-                current -= 10;
-                carry = 1;
-            } else {
+            digits[idx] =  (digits[idx] + carry) % 10;
+            if(digits[idx] != 0) {
                 carry = 0;
-            }
-            result[idx] = current;
-            add = 0;           
+                break;
+            }    
         }
-        
         
         if(carry == 1) {
-            int [] temp = new int [result.length + 1];
-            temp[0] = 1;
-            for(int idx = 1; idx < temp.length; idx ++) {
-                temp[idx] = result[idx-1];
-            }
-            return temp;
-            
+            digits = new int [digits.length + 1];
+            digits[0] = 1;
         }
         
-        return result;
+        return digits;
 
     }
 }
