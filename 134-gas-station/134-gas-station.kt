c80608aec2fcfe62839cb18e542a358c3fefeed3
이재석 <34270397+seok-2-o@users.noreply.github.com>
2022-08-18 21:19:57 +0900

    Time: 576 ms (68.33%), Space: 73.2 MB (81.67%) - LeetHub

---
+++ 134-gas-station/134-gas-station.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun canCompleteCircuit(gas: IntArray, cost: IntArray): Int {
+        
+    var total = 0
+    var starting = 0
+    var curr = 0
+        
+    for (idx in 0..gas.size - 1) {
+        curr += gas[idx] - cost[idx]
+        total += gas[idx] - cost[idx]
+        if (curr < 0) {
+            starting = idx + 1
+            curr = 0
+        }
+    }
+        return if (total >= 0) starting else -1
+        
+    }
+}
